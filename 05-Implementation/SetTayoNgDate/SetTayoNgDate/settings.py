10b20bc020f0141791f8600cbd051359a0142f93
KneeSoxx <KneeSoxx@github.com>
2017-02-02 15:34:30 +0800

    Add db file and settings file

--- 05-Implementation/SetTayoNgDate/SetTayoNgDate/settings.py
+++ 05-Implementation/SetTayoNgDate/SetTayoNgDate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #'django.contrib.admin',
+    'std.apps.StdConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
