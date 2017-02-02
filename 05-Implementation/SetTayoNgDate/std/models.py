65bd4f5d69569c3582cf9097c3b5d39fa963c617
KneeSoxx <KneeSoxx@github.com>
2017-02-02 16:34:00 +0800

    Add Schedule model

--- 05-Implementation/SetTayoNgDate/std/models.py
+++ 05-Implementation/SetTayoNgDate/std/models.py
@@ -6,3 +6,11 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=256)
+
+class Schedule(models.Model):
+    # todo, make foreign keysss
+    user_id = models.IntegerField()
+    # 0 = Sunday to 6 = Saturday
+    day = models.IntegerField()
+    # 0 = 00:00 - 00:59 to 23 = 23:00 - 23:99
+    hour = models.IntegerField()
