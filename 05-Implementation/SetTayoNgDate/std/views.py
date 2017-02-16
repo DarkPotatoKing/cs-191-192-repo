17127dc760eaf713b789667d8129620558995ab9
Edward James Bariring <ejbariring@gmail.com>
2017-02-17 06:03:38 +0800

    Added documents

--- 05-Implementation/SetTayoNgDate/std/views.py
+++ 05-Implementation/SetTayoNgDate/std/views.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2017 Manifold Cheddar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2016-2017.
+
+"""
+#2/10/17 - Edward James Bariring - finished doing basic views; more to be added
+#2/16/17 - Edward James Bariring - added methods for authentication
+
 from django.shortcuts import render, redirect
 from std.models import *
 # Create your views here.
@@ -20,6 +35,7 @@ def login(request):
 
 
 def register(request):
+    #to do: add error detection for user creation
     User.create(request.POST.get('uname'), request.POST.get('passw'))
     return render(request, 'std/index.html')
 
