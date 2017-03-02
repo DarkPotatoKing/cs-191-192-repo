cfff9a1656640ab1cb840e64225256ffcd8c9545
lendmeEnergy <arvinbandong@gmail.com>
2017-03-02 15:40:09 +0800

    Added CSS for login page

--- 05-Implementation/SetTayoNgDate/SetTayoNgDate/urls.py
+++ 05-Implementation/SetTayoNgDate/SetTayoNgDate/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^std/', include('std.urls')),
+    url(r'', include('std.urls')),
     # url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     # url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     # url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
