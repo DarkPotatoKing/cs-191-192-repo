72770fd37d7654e26e3bb4b9019dd7a4d5722e42
DarkPotatoKing <kyle.rosales97@gmail.com>
2017-03-27 11:44:22 +0800

    Add name field to User model

--- 05-Implementation/SetTayoNgDate/std/models.py
+++ 05-Implementation/SetTayoNgDate/std/models.py
@@ -23,6 +23,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
 
     def __str__(self):
         return self.username + ' ' + self.password
